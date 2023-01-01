a983028de888ba2d56f84459cab80593ace4bfa3
sssv587 <1092663295@qq.com>
2023-01-01 22:39:15 +0800

    用户授权-自定义403页面

--- security-demo1/src/main/java/com/futurebytedance/securitydemo1/config/SecurityConfigTest.java
+++ security-demo1/src/main/java/com/futurebytedance/securitydemo1/config/SecurityConfigTest.java
@@ -28,6 +28,9 @@ public class SecurityConfigTest extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        //配置没有权限访问跳转自定义页面
+        http.exceptionHandling().accessDeniedPage("/unauth.html");
+
         http.formLogin() //自定义自己编写的登录页面
                 .loginPage("/login.html") //登录页面设置
                 .loginProcessingUrl("/user/login") //登录访问路径
@@ -40,9 +43,9 @@ public class SecurityConfigTest extends WebSecurityConfigurerAdapter {
                 //2.hasAnyAuthority方法
                 //.antMatchers("/test/index").hasAnyAuthority("admins,manager")
                 //3.hasRole方法
-                //.antMatchers("/test/index").hasRole("sale")
+                .antMatchers("/test/index").hasRole("sale")
                 //4.hasAnyRole方法
-                .antMatchers("/test/index").hasAnyRole("sale,admin")
+                //.antMatchers("/test/index").hasAnyRole("sale,admin")
                 .anyRequest().authenticated()
                 .and().csrf().disable(); //关闭csrf防护
     }
