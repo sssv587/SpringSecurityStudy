c502cfba27b09e857e373baabe85ab0e6760ca52
sssv587 <1092663295@qq.com>
2023-01-02 17:16:17 +0800

    用户授权-用户注销

--- security-demo1/src/main/java/com/futurebytedance/securitydemo1/config/SecurityConfigTest.java
+++ security-demo1/src/main/java/com/futurebytedance/securitydemo1/config/SecurityConfigTest.java
@@ -28,13 +28,16 @@ public class SecurityConfigTest extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        //退出
+        http.logout().logoutUrl("/logout").logoutSuccessUrl("/test/hello").permitAll();
+
         //配置没有权限访问跳转自定义页面
         http.exceptionHandling().accessDeniedPage("/unauth.html");
 
         http.formLogin() //自定义自己编写的登录页面
                 .loginPage("/login.html") //登录页面设置
                 .loginProcessingUrl("/user/login") //登录访问路径
-                .defaultSuccessUrl("/test/index").permitAll() //登陆成功之后,跳转路径
+                .defaultSuccessUrl("/success.html").permitAll() //登陆成功之后,跳转路径
                 .and().authorizeRequests()
                 .antMatchers("/", "/test/hello", "/user/login").permitAll() //设置哪些路径可以直接访问,不需要认证
                 //当前登录用户，只有具有admins权限才可以访问这个路径
