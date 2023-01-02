d9f8085da7a66520ca5d42fc365220b531de93df
sssv587 <1092663295@qq.com>
2023-01-02 18:55:33 +0800

    CSRF功能

---
+++ security-csrf/src/test/java/com/futurebytedance/securitycsrf/SecurityCsrfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futurebytedance.securitycsrf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityCsrfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
