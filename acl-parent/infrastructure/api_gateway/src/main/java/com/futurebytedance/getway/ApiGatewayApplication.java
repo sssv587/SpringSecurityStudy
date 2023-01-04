52bcd320848b9995751033fb28e1dfb626dfda9b
sssv587 <1092663295@qq.com>
2023-01-05 00:22:45 +0800

    微服务权限案例-整合网关

---
+++ acl-parent/infrastructure/api_gateway/src/main/java/com/futurebytedance/getway/ApiGatewayApplication.java
@@ -0,0 +1,19 @@
+package com.futurebytedance.getway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author yuhang.sun
+ * @version 1.0
+ * @date 2023/1/5 - 0:09
+ * @Description
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
---
+++ acl-parent/infrastructure/api_gateway/src/main/java/com/futurebytedance/getway/config/CorsConfig.java
@@ -0,0 +1,31 @@
+package com.futurebytedance.getway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+/**
+ * @author yuhang.sun
+ * @version 1.0
+ * @date 2023/1/5 - 0:11
+ * @Description
+ */
+@Configuration
+public class CorsConfig {
+    //解决跨域
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
